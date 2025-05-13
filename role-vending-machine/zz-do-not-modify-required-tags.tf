933e7d6df2b6b98bdd7dc17627cc8409a0971f77
Benjamin Morris <93620006+bmorrissirromb@users.noreply.github.com>
2025-05-13 09:44:17 -0700

    format

--- role-vending-machine/zz-do-not-modify-required-tags.tf
+++ role-vending-machine/zz-do-not-modify-required-tags.tf
@@ -5,11 +5,11 @@
 
 # REQUIRED TAGS - Add values to each before deploying your Terraform code.
 locals {
-  Owner       = "bmorris-contractor@me.com"       # Email address of the business owner GROUP. This should NOT be a named individual. It should be a distribution list email.
-  Deployment  = "Terraform"                                               # one of: ag, tempus
+  Owner       = "bmorris-contractor@me.com"                        # Email address of the business owner GROUP. This should NOT be a named individual. It should be a distribution list email.
+  Deployment  = "Terraform"                                        # one of: ag, tempus
   Environment = "dev"                                              # Must be one of: dev, tst, stg, prd. Can be pulled dynamically from variables if you deploy to multiple environments and specify config via a tfvars file.
   Project     = "platform"                                         # Name of the application or service this resource belongs to. (eg. secondary-analysis, ap, cancernext, cardio, exome, skm, tracker, monitoring, adaudit, overhead)
-  Criticality = "dev"                                             # one of: tier1, tier2, tier3, none. tier1 is most critical.
+  Criticality = "dev"                                              # one of: tier1, tier2, tier3, none. tier1 is most critical.
   Repo        = "https://github.com/ambrygen/role-vending-machine" # The URL of the IAC repo that contains this application's code. (eg. https://github.com/<MY_GITHUB_ORG>/i_copied_the_default_value)
 
   # OPTIONAL TAGS - These tags are not required globally but can be added as needed
@@ -35,12 +35,12 @@ module "tagging_module" {
   # For details on exact tag validation, see the repo below
   source = "github.com/sirromb-bootlab/terraform-aws-tagging-module//terraform?ref=v1.0.0"
   # Required tags
-      Project     = local.Project     # Which platform/project created this infra
-      Deployment  = local.Deployment  # Mechanism used to deploy the resource (eg. Terraform)
-      Environment = local.Environment # Environment in which the resource is deployed. One of: dev, preprod, prod
-      Repo        = local.Repo        # URL of the repository where the Infrastructure as Code is sourced from.
-      Criticality = local.Criticality # The level of criticality for the application. Informs RPO and RTO. One of: critical, prod-non-critical, internal, dev
-      Owner       = local.Owner       # Email address of the owner or owner distribution list.
+  Project     = local.Project     # Which platform/project created this infra
+  Deployment  = local.Deployment  # Mechanism used to deploy the resource (eg. Terraform)
+  Environment = local.Environment # Environment in which the resource is deployed. One of: dev, preprod, prod
+  Repo        = local.Repo        # URL of the repository where the Infrastructure as Code is sourced from.
+  Criticality = local.Criticality # The level of criticality for the application. Informs RPO and RTO. One of: critical, prod-non-critical, internal, dev
+  Owner       = local.Owner       # Email address of the owner or owner distribution list.
 
   # Optional tags
   optional_tags = local.optional_tags
